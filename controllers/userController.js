72199d896b7a2a0265e63f49279b2fb6c62e7065
fikireab-m <fikireab.meku@gmail.com>
2024-02-26 22:13:53 +0300

    error handler middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,12 @@
+import asyncHandler from "express-async-handler";
 /**
  * @desc Auth user / set token
  * @param {*} req 
  * @param {*} res 
  * @route api/users/auth
  */
-export const authUser = (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
+    // res.status(401);
+    // throw new Error('Something went wrong');
     res.status(200).json({ message: 'User authorized' })
-}
+})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -8,5 +9,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use('/api/users', userRoutes);
 app.get("/", (req, res) => res.send('Welcome'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));
