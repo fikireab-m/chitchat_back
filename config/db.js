ff0ff09be96dae81cd52e373dd3d5970e072b4f0
fikireab-m <fikireab.meku@gmail.com>
2024-02-27 11:24:15 +0300

    db conn

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+export const connectDB = async () => {
+    dotenv.config();
+
+    const uri = process.env.MONGO_URI;
+    console.log(uri)
+    try {
+        await mongoose.connect(uri).then((con) => {
+            console.log(`MongoDB connected - ${con.connection.host}`);
+        });
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+};
