818ad70ff587cdd493dc2ab1339d4990e7e04205
fikireab-m <fikireab.meku@gmail.com>
2024-04-05 10:23:17 +0300

    Integrating adminBro 	new file:   .adminbro/.entry.js 	new file:   dashboard.jsx 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

---
+++ dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "admin-bro";
+
+const Dashboard = (props) => {
+  return <Box>Welcome back</Box>;
+};
+
+export default Dashboard;
--- index.js
+++ index.js
@@ -2,36 +2,68 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
-import { connectDB } from "./config/db.js";
-
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import path from "path";
 import { fileURLToPath } from 'url';
+import AdminBro from "admin-bro";
+import AdminBroExpress from '@admin-bro/express';
+import AdminBroMongoose from '@admin-bro/mongoose';
+
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import { connectDB } from "./config/db.js";
+import User from "./models/user.js";
+import Post from "./models/post.js";
+import Comment from "./models/comment.js";
+import Reply from "./models/reply.js";
+
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-
 connectDB();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const AdminBroOptions = {
+    rootPath: '/admin',
+    resources: [
+        { resource: User, options: { navigation: { name: 'Resources' } } },
+        { resource: Post, options: { navigation: { name: "Resources" } } },
+        { resource: Comment, options: { navigation: { name: "Resources" } } },
+        { resource: Reply, options: { navigation: { name: "Resources" } } },
+    ],
+    // dashboard: {
+    //     component: AdminBro.bundle('./dashboard.jsx')
+    // },
+    branding: {
+        companyName: "The Chitchat Cafe"
+    }
+}
+const adminBro = new AdminBro(AdminBroOptions);
+
+const router = AdminBroExpress.buildRouter(adminBro);
+
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.get("/", async (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "/socket.io.html"));
 });
 
+app.use(adminBro.options.rootPath, router)
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
